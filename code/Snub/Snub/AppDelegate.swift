b084844a27c445340edd2870dbccee1eb23fd2d5
Ashok Gelal <ashok@metageek.net>
2015-10-13 22:22:34 -0600

    Setup project

---
+++ code/Snub/Snub/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Snub
+//
+//  Created by Ashok Gelal on 10/13/15.
+//  Copyright © 2015 RnA Apps. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
