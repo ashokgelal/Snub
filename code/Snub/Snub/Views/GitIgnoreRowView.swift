6d876d56524a3ea59a1e30764724278937e3973a
Rachana Acharya <rasnacharya@gmail.com>
2015-10-16 14:51:52 -0600

    Add outlet connections for dropdown menu item

---
+++ code/Snub/Snub/Views/GitIgnoreRowView.swift
@@ -0,0 +1,32 @@
+//
+//  GitIgnoreRowView.swift
+//  Snub
+//
+//  Created by Rachana Acharya on 10/16/15.
+//  Copyright © 2015 RnA Apps. All rights reserved.
+//
+
+import Cocoa
+
+class GitIgnoreRowView: NSTableCellView {
+    @IBOutlet weak var addMenuItem: NSMenuItem!
+    @IBOutlet weak var replaceMenuItem: NSMenuItem!
+    @IBOutlet weak var appendMenuItem: NSMenuItem!
+
+}
+
+// Mark: Menu Actions
+extension GitIgnoreRowView {
+    
+    @IBAction func add(sender: AnyObject?) {
+        DDLogError("Boom!")
+    }
+    
+    @IBAction func replace(sender: AnyObject?) {
+        DDLogError("Boom! Replace")
+    }
+    
+    @IBAction func append(sender: AnyObject?) {
+        DDLogError("Boom! Replace")
+    }
+}
