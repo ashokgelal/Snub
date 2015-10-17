c74a75b230bd43a24efe8159310ee9eacbefaf75
Ashok Gelal <ashokgelal@gmail.com>
2015-10-17 13:06:21 -0600

    Add JetBrains/IntelliJ project detector

---
+++ code/Snub/Snub/Services/ProjectDetectors/JetBrainsProjectDetector.swift
@@ -0,0 +1,19 @@
+//
+//  JetBrainsProjectDetector.swift
+//  Snub
+//
+//  Created by Rachana Acharya on 10/17/15.
+//  Copyright © 2015 RnA Apps. All rights reserved.
+//
+
+class JetBrainsProjectDetector: ProjectTypeDetector {
+    func detect(fileExtensions: [String]) -> ProjectDetectionResult? {
+        let knownExtensions = ["iml", "idea", "idea_modules"]
+        let detectedTypes = fileExtensions.filter { return knownExtensions.contains($0) }
+        if detectedTypes.count > 0 {
+            DDLogVerbose("Detected JetBrains/IntelliJ project type \(detectedTypes.count)")
+            return ProjectDetectionResult(projectType: "JetBrains/IntelliJ", confidencePercent: Double(detectedTypes.count) * 100.0/Double(knownExtensions.count))
+        }
+        return nil
+    }
+}
