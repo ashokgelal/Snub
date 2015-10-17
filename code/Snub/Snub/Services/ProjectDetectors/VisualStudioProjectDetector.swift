140790a1633021d761f53e45bf7286183285106c
Rachana Acharya <rasnacharya@gmail.com>
2015-10-17 16:01:59 -0600

    Implement VisualStudio project detector

---
+++ code/Snub/Snub/Services/ProjectDetectors/VisualStudioProjectDetector.swift
@@ -0,0 +1,19 @@
+//
+//  VisualStudioProjectDetector.swift
+//  Snub
+//
+//  Created by Rachana Acharya on 10/17/15.
+//  Copyright © 2015 RnA Apps. All rights reserved.
+//
+
+class VisualStudioProjectDetector: ProjectTypeDetector {
+    func detect(fileExtensions: [String]) -> ProjectDetectionResult? {
+        let knownExtensions = ["vs", "sln", "csproj"]
+        let detectedTypes = fileExtensions.filter { return knownExtensions.contains($0) }
+        if detectedTypes.count > 0 {
+            DDLogVerbose("Detected \(detectedTypes.count) Visual Studio project type")
+            return ProjectDetectionResult(projectType: "Visual Studio", confidencePercent: Double(detectedTypes.count) * 100.0/Double(knownExtensions.count))
+        }
+        return nil
+    }
+}
