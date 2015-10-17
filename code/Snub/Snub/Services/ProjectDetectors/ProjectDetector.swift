b99a6d1e563555bd3e97ca6d62ace66f7964c272
Ashok Gelal <ashokgelal@gmail.com>
2015-10-17 13:07:48 -0600

    Start detecting Xcode and JetBrains/IntelliJ project types

---
+++ code/Snub/Snub/Services/ProjectDetectors/ProjectDetector.swift
@@ -0,0 +1,29 @@
+//
+//  ProjectDetector.swift
+//  Snub
+//
+//  Created by Ashok Gelal on 10/17/15.
+//  Copyright © 2015 RnA Apps. All rights reserved.
+//
+
+import Cocoa
+
+class ProjectDetector {
+    static let instance = ProjectDetector()
+    private var detectors: [ProjectTypeDetector] = []
+    private init() {
+        addDetectors()
+    }
+    
+    private func addDetectors() {
+        detectors.append(XcodeProjectDetector())
+        detectors.append(JetBrainsProjectDetector())
+    }
+    
+    func identify(url: NSURL) throws -> [String] {
+        let fm = NSFileManager.defaultManager()
+        let subs = try fm.subpathsOfDirectoryAtPath(url.path!).map { ($0 as NSString).pathExtension }
+        let results = detectors.map { $0.detect(subs) }.filter { $0 != nil }
+        return results.sort{ $0!.confidencePercent > $1!.confidencePercent }.map { $0!.projectType }
+    }
+}
