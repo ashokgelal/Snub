0e578fb10c86570e8b39ae23b76d91c371060def
Ashok Gelal <ashokgelal@gmail.com>
2015-10-18 20:20:29 -0600

    Cleanup extensions

--- code/Snub/Extensions/Extensions.swift
+++ code/Snub/Extensions/Extensions.swift
@@ -8,44 +8,6 @@
 
 import Foundation
 
-// MARK: NSFileManager Extensions
-extension NSFileManager {
-    func checkIfDirectoryExists(path: String) -> Bool {
-        let fm = NSFileManager.defaultManager()
-        var isDir: ObjCBool = false
-        let fileExists = fm.fileExistsAtPath(path, isDirectory: &isDir)
-        return fileExists && isDir
-    }
-    
-    func checkIfFileExists(path: String) -> Bool {
-        let fm = NSFileManager.defaultManager()
-        var isDir: ObjCBool = false
-        let fileExists = fm.fileExistsAtPath(path, isDirectory: &isDir)
-        return fileExists && !isDir
-    }
-    
-    func getApplicationDirectoryPath() -> String {
-        let paths = NSSearchPathForDirectoriesInDomains(.ApplicationSupportDirectory, .UserDomainMask, true)
-        let supportDirectory = paths.first!
-        return (supportDirectory as NSString).stringByAppendingPathComponent(MagicStrings.APPNAME)
-    }
-}
-
-// MARK: String Extensions
-extension String {
-    func fileName() -> String {
-        return ((self as NSString).lastPathComponent as NSString).stringByDeletingPathExtension
-    }
-    
-    func fileExtension() -> String {
-        return (self as NSString).pathExtension
-    }
-    
-    func appendPathComponent(suffix: String) -> String {
-        return (self as NSString).stringByAppendingPathComponent(suffix)
-    }
-}
-
 // MARK: Thread Functions
 func delay(delay: Double, closure:()->()) {
     dispatch_after(
