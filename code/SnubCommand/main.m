ef3b9bc7a1a19874134114fb5870fc931ebc32dc
Ashok Gelal <ashokgelal@gmail.com>
2015-10-18 20:39:22 -0600

    Add SnubCommand project

---
+++ code/SnubCommand/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SnubCommand
+//
+//  Created by Ashok Gelal on 10/18/15.
+//  Copyright © 2015 RnA Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
